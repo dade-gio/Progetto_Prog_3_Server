42cf62178ca207be0ab2ec04330f140f04c8358f
nis04 <153619180+nis04@users.noreply.github.com>
2024-11-14 21:40:19 +0100

    Add files via upload

--- src/Email.java
+++ src/Email.java
@@ -1,47 +1,105 @@
+
+
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 public class Email implements Serializable {
-    private static final long serialVersionUID = 1L;
-    private String id;
-    private String sender;
-    private List<String> recipients;
-    private String subject;
-    private String body;
-    private Date sentDate;
+    private ArrayList<String> destsEmail = new ArrayList<>();
+    private String mittEmail ,dest, argEmail, testoEmail;
+    private int priorEmail;
+    private Date dataSpedEmail;
+    boolean isRead;
 
-    public Email(String id, String sender, List<String> recipients, String subject, String body, Date sentDate) {
-        this.id = id;
-        this.sender = sender;
-        this.recipients = recipients;
-        this.subject = subject;
-        this.body = body;
-        this.sentDate = sentDate;
+    /*TODO: modificare il campo destinatari! deve essere una lista (ArrayList preferibilmente) di stringhe*/
+
+    public Email(String mitt, String dest, ArrayList<String> dests, String arg, String testo, int prior, Date dataSped, boolean isRead){
+        this.mittEmail = mitt;
+        this.dest = dest;
+        this.destsEmail = dests;
+        this.argEmail = arg;
+        this.testoEmail = testo;
+        this.priorEmail = prior;
+        this.dataSpedEmail = dataSped;
+        this.isRead = isRead;
     }
 
-    // Getters per tutti i campi
-    public String getId() {
-        return id;
+    public boolean isRead() {
+        return isRead;
     }
 
-    public String getSender() {
-        return sender;
+    public void setRead(boolean isRead) {
+        this.isRead = isRead;
     }
 
-    public List<String> getRecipients() {
-        return recipients;
+    public ArrayList<String> getDestEmail() {
+        return destsEmail;
     }
 
-    public String getSubject() {
-        return subject;
+    public String getDest() {
+        return dest;
     }
 
-    public String getBody() {
-        return body;
+    public String getArgEmail() {
+        return argEmail;
     }
 
-    public Date getSentDate() {
-        return sentDate;
+    public void setArgEmail(String argEmail) {
+        this.argEmail = argEmail;
+    }
+
+    public String getTestoEmail() {
+        return testoEmail;
+    }
+
+    public void setTestoEmail(String testoEmail) {
+        this.testoEmail = testoEmail;
+    }
+
+    public int getPriorEmail() {
+        return priorEmail;
+    }
+
+    public void setPriorEmail(int priorEmail) {
+        this.priorEmail = priorEmail;
+    }
+
+    public Date getDataSpedEmail() {
+        return dataSpedEmail;
+    }
+
+    public void setDataSpedEmail(Date dataSpedEmail) {
+        this.dataSpedEmail = dataSpedEmail;
+    }
+
+
+    public String getMittEmail() {
+        return mittEmail;
+    }
+
+    public String getCcString(){
+        String res="";
+        for(String iter:destsEmail){
+            res=res+iter+",";
+        }
+        if (!res.equals("") && res.length() > 0 ) {
+            res = res.substring(0, res.length()-1);
+        }
+        return res;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Email mail = (Email)o;
+        if(this.mittEmail.equals(mail.getMittEmail()) &&
+                this.destsEmail.equals(mail.getDestEmail()) &&
+                this.argEmail.equals(mail.getArgEmail()) &&
+                this.testoEmail.equals(mail.getTestoEmail())){return true;}
+        else return false;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
     }
 }
